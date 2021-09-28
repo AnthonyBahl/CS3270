e9ff0b6d810fd8264f1caa6a808ced6bd4a14908
AnthonyBahlWeber <77309481+AnthonyBahlWeber@users.noreply.github.com>
2021-09-28 12:54:52 -0600

    added a menu to the app, it doesn't do much though

--- Week5sum/app/src/main/java/edu/weber/cs/w01113559/week5sum/MainActivity.java
+++ Week5sum/app/src/main/java/edu/weber/cs/w01113559/week5sum/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
     }
 }
