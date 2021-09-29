46627004b5b8c4571d9a75a19ae9628f3730d565
AnthonyBahlWeber <77309481+AnthonyBahlWeber@users.noreply.github.com>
2021-09-29 01:22:05 -0600

    Added Change Results fragment XML

---
+++ CS3270A5/app/src/main/java/edu/weber/cs/w01113559/cs3270a5/ChangeResults.java
@@ -0,0 +1,31 @@
+package edu.weber.cs.w01113559.cs3270a5;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChangeResults extends Fragment {
+
+    private View root;
+
+    /**
+     * Constructor
+     */
+    public ChangeResults() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return root = inflater.inflate(R.layout.fragment_change_results, container, false);
+    }
+}
