f4e24ac2853ae42c7dab0a0b7192d53ab9a32cab
AnthonyBahlWeber <77309481+AnthonyBahlWeber@users.noreply.github.com>
2021-09-29 14:28:14 -0600

    Created the necessary fragments

---
+++ CS3270A5/app/src/main/java/edu/weber/cs/w01113559/cs3270a5/ChangeActions.java
@@ -0,0 +1,28 @@
+package edu.weber.cs.w01113559.cs3270a5;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChangeActions extends Fragment {
+
+    private View root;
+
+    public ChangeActions() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return root = inflater.inflate(R.layout.fragment_change_actions, container, false);
+    }
+}
---
+++ CS3270A5/app/src/main/java/edu/weber/cs/w01113559/cs3270a5/ChangeButtons.java
@@ -0,0 +1,28 @@
+package edu.weber.cs.w01113559.cs3270a5;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChangeButtons extends Fragment {
+
+    private View root;
+
+    public ChangeButtons() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return root = inflater.inflate(R.layout.fragment_change_buttons, container, false);
+    }
+}
