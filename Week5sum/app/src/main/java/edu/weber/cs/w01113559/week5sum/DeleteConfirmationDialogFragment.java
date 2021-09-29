e81d565bbbce1c42e9bfa74e4d0a933cf28ff0e5
AnthonyBahlWeber <77309481+AnthonyBahlWeber@users.noreply.github.com>
2021-09-28 22:00:46 -0600

    Added Dialog information

---
+++ Week5sum/app/src/main/java/edu/weber/cs/w01113559/week5sum/DeleteConfirmationDialogFragment.java
@@ -0,0 +1,49 @@
+package edu.weber.cs.w01113559.week5sum;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DeleteConfirmationDialogFragment extends DialogFragment {
+
+    public DeleteConfirmationDialogFragment() {
+        // Required empty public constructor
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        // Build the dialog, but to do that we have to get the activity because it is going to be above everything in the activity
+        // not just the fragment itself.
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.delete_warning)
+                .setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //ToDo: Do your deletion logic
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //ToDo: Cancel logic
+                    }
+                });
+
+        return builder.create();
+    }
+}
--- Week5sum/app/src/main/java/edu/weber/cs/w01113559/week5sum/MainActivity.java
+++ Week5sum/app/src/main/java/edu/weber/cs/w01113559/week5sum/MainActivity.java
@@ -1,10 +1,15 @@
 package edu.weber.cs.w01113559.week5sum;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button btnShow = (Button) findViewById(R.id.btnShow);
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DeleteConfirmationDialogFragment dialog = new DeleteConfirmationDialogFragment();
+                dialog.setCancelable(false);
+                dialog.show(getSupportFragmentManager(), "delete_dialog");
+            }
+        });
     }
 
     @Override
@@ -24,5 +39,29 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main, menu);
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.action_done:
+                //USER PRESSED DONE
+                action_done();
+                return true;
+            case R.id.action_settings:
+                //USER PRESSED SETTINGS
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * Creates a toast stating that the user pressed done.
+     */
+    private void action_done() {
+        Toast toast = Toast.makeText(this, "Done Pressed", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
