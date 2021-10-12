971a7ae8075145bc63df19b6b5486df565b532b4
Anthony Bahl <anthonybahl@mail.weber.edu>
2021-10-11 22:11:30 -0600

    RoomDemo2 Added

---
+++ RoomDemo2/app/src/main/java/edu/weber/cs/w01113559/db/AppDatabase.java
@@ -0,0 +1,27 @@
+package edu.weber.cs.w01113559.db;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {User.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase instance;
+
+    // Create the database or return it if it already exists
+    public static AppDatabase getInstance(Context context){
+
+        if (instance != null)
+            return instance;
+
+        instance = Room.databaseBuilder(context, AppDatabase.class, "user-database")
+                .build();
+        return instance;
+    }
+
+    public abstract UserDAO userDAO();
+
+}
---
+++ RoomDemo2/app/src/main/java/edu/weber/cs/w01113559/db/UserDAO.java
@@ -0,0 +1,27 @@
+package edu.weber.cs.w01113559.db;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface UserDAO {
+
+    @Query("Select * from user")
+    List<User> getAll();
+
+    @Query("Select * from user where uid=:uid LIMIT 1")
+    User getUserByID(int uid);
+
+    @Query("Select * from user where first_name LIKE :first AND last_name LIKE :last LIMIT 1")
+    User findByName(String first, String last);
+
+    @Insert
+    void insertAll(User... users);
+
+    @Delete
+    void deleteUser(User user);
+}
