4f6a8dfe185dd7b405a68c092cdecd372822f8a5
Anthony Bahl <anthonybahl@mail.weber.edu>
2021-10-20 22:31:32 -0600

    Removed Navigation Modified fab to a +

--- CS3270A8/app/src/main/java/edu/weber/cs/w01113559/cs3270a8/MainActivity.java
+++ CS3270A8/app/src/main/java/edu/weber/cs/w01113559/cs3270a8/MainActivity.java
@@ -8,30 +8,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 import edu.weber.cs.w01113559.cs3270a8.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        edu.weber.cs.w01113559.cs3270a8.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,12 +28,5 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
---
+++ CS3270A8/app/src/main/java/edu/weber/cs/w01113559/cs3270a8/db/AppDatabase.java
@@ -0,0 +1,25 @@
+package edu.weber.cs.w01113559.cs3270a8.db;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Course.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase instance;
+
+    public static AppDatabase getInstance(Context context) {
+        if (instance != null)
+            return instance;
+
+        instance = Room.databaseBuilder(context, AppDatabase.class, "course-database").build();
+
+        return instance;
+
+    }
+
+    public abstract CourseDAO courseDAO();
+}
