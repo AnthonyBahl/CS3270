ea13379d9c70e4d09bf106fcc66e4e423ee99380
Anthony Bahl <anthonybahl@mail.weber.edu>
2021-10-22 02:27:07 -0600

    Added Save fab button functionality.

--- CS3270A8/app/src/main/java/edu/weber/cs/w01113559/cs3270a8/MainActivity.java
+++ CS3270A8/app/src/main/java/edu/weber/cs/w01113559/cs3270a8/MainActivity.java
@@ -14,9 +14,14 @@ import edu.weber.cs.w01113559.cs3270a8.db.Course;
 
 public class MainActivity extends AppCompatActivity implements CourseListFragment.onCourseClickListener {
 
+    private FragmentManager fragmentManager;
+    private CourseEditFragment courseEditFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        fragmentManager = getSupportFragmentManager();
 
         edu.weber.cs.w01113559.cs3270a8.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -34,15 +39,20 @@ public class MainActivity extends AppCompatActivity implements CourseListFragmen
         binding.fabSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                // Make sure CourseEditFragment isn't null
+                if (courseEditFragment == null) {
+                    courseEditFragment = (CourseEditFragment) fragmentManager.findFragmentByTag("courseEditFrag");
+                }
+
+                courseEditFragment.saveCourse();
             }
         });
     }
 
     @Override
     public void courseClicked(Course course) {
-        getSupportFragmentManager().beginTransaction()
+        fragmentManager.beginTransaction()
                 .replace(R.id.mainContentFragment, new CourseEditFragment(course), "courseEditFrag")
                 .commit();
     }
